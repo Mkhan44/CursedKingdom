741b3646e1cbc68923c6f1d6d5150274cc614327
Mohamed R. Khan <khanantisora@hotmail.com>
2022-05-31 23:44:49 -0400

    Added hover animation to card
    Just a small hover animation to the card when hovered over like most other card games do.

--- Cursed Kingdom/Assets/Scripts/CardRelated/Base/CardDisplay.cs
+++ Cursed Kingdom/Assets/Scripts/CardRelated/Base/CardDisplay.cs
@@ -7,8 +7,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class CardDisplay : MonoBehaviour
+public class CardDisplay : MonoBehaviour , IPointerEnterHandler , IPointerDownHandler , IPointerExitHandler
 {
     //Test.
     public CardData testCard;
@@ -17,6 +18,30 @@ public class CardDisplay : MonoBehaviour
     [SerializeField] private Image cardImage;
     [SerializeField] private TextMeshProUGUI cardNameText;
     [SerializeField] private TextMeshProUGUI cardNumberText;
+
+    //Test Interactions.
+    [SerializeField] private bool hoveredOverCard;
+
+    [SerializeField] private Vector3 originalSize;
+    [SerializeField] private Vector3 hoveredSized;
+
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        hoveredOverCard = true;
+        StartCoroutine(HoverCardEffect());
+    }
+
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        hoveredOverCard = false;
+        StartCoroutine(LeaveCardEffect());
+    }
+
     protected virtual void Start()
     {
         if(testCard is MovementCardData)
@@ -46,5 +71,35 @@ public class CardDisplay : MonoBehaviour
         
         cardNameText.text = newCard.CardName.ToString();
         cardImage.sprite = newCard.CardSprite;
+    }
+
+    public IEnumerator HoverCardEffect()
+    {
+        float timePassed = 0.0f;
+        float rate = 0.0f;
+
+        rate = 1.0f / 2.0f * 3.0f;
+        while (hoveredOverCard)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, hoveredSized, timePassed / rate);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+        yield return null;
+    }
+
+    public IEnumerator LeaveCardEffect()
+    {
+        float timePassed = 0.0f;
+        float rate = 0.0f;
+
+        rate = 1.0f / 2.0f * 3.0f;
+        while (!hoveredOverCard)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, originalSize, timePassed / rate);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+        yield return null;
     }
 }
