34d2aa190fb67f1b55c04a5de0537a4ee5c9bc79
TimourKhan <timour789@gmail.com>
2023-05-21 21:45:00 -0400

    Edited tween for victory

--- Cursed Kingdom/Assets/Scripts/Tweens/Tween_Victory.cs
+++ Cursed Kingdom/Assets/Scripts/Tweens/Tween_Victory.cs
@@ -6,12 +6,14 @@ public class Tween_Victory : MonoBehaviour
 {
     public GameObject panel;
     public GameObject victory;
+    public CanvasGroup victoryCanvas;
 
     public float startX = -2500f;
     public float endX = -1000f;
     public Vector3 startScale = new Vector3(100f, 100f, 100f);
     public float moveDuration = 0.5f;
-    private float scaleDuration = 1f;
+    public float scaleDuration = 1f;
+    public float fadeDuration = 1f;
 
     private void OnEnable()
     {
@@ -32,16 +34,22 @@ public class Tween_Victory : MonoBehaviour
         // Move the artwork from the starting position to the target position
         LeanTween.moveLocal(panel, targetPosition, moveDuration).setEase(LeanTweenType.easeOutQuad).setOnComplete(ScaleVictory);
 
+        // Set up text to be scaled and faded in
         victory.transform.localScale = startScale;
+        victoryCanvas = victory.GetComponent<CanvasGroup>();
+        victoryCanvas.alpha = 0f;
+        
     }
 
     private void ScaleVictory()
     {
-        
+        // Define target scale of 1
         Vector3 targetScale = new Vector3(1f, 1f, 1f);
 
+        LeanTween.alphaCanvas(victoryCanvas, 1f, fadeDuration);
         LeanTween.scale(victory, targetScale, scaleDuration)
             .setEase(LeanTweenType.easeOutExpo)
             .setFrom(startScale);
     }
+
 }
