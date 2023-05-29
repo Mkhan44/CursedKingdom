74a5d4aa01d5ae11b5c63383d3b117001c683cd1
Mohamed R. Khan <khanantisora@hotmail.com>
2023-05-29 00:29:13 -0400

    Fixed editor script bug

--- Cursed Kingdom/Assets/Scripts/MapManager.cs
+++ Cursed Kingdom/Assets/Scripts/MapManager.cs
@@ -5,7 +5,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class MapManager : MonoBehaviour
 {
