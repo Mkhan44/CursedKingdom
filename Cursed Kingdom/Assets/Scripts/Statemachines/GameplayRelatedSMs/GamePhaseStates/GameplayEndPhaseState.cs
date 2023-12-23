ad48a5b83a1491a85c1a6503793b948d3ab574ef
Mohamed R. Khan <mohamed.bukugames@gmail.com>
2023-12-23 17:26:05 -0500

    Got rid of using statement that stopped builds
    Fixed an auto added using statement that prevented builds.

--- Cursed Kingdom/Assets/Scripts/Statemachines/GameplayRelatedSMs/GamePhaseStates/GameplayEndPhaseState.cs
+++ Cursed Kingdom/Assets/Scripts/Statemachines/GameplayRelatedSMs/GamePhaseStates/GameplayEndPhaseState.cs
@@ -4,7 +4,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class GameplayEndPhaseState : BaseState
 {
